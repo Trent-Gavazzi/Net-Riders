16bad3a568430fadf6c01d5609e3979a6e5d038d
ECarty-01 <lokewar81@gmail.com>
2026-04-16 16:20:21 -0400

    finished up, added end

--- Assets/scripts/VaultInteractable.cs
+++ Assets/scripts/VaultInteractable.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class VaultInteractable : MonoBehaviour, IInteractable
 {
-    
+    public SceneLoader sceneLoader;
     public void Interact()
     {
-
+        sceneLoader.LoadScene("End");
     }
 }
